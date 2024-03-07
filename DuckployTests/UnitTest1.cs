f754cea41674393fd6ff3263bb8208e943db45a1
Jordan Kniest <mail@jkniest.de>
2024-03-07 14:38:33 +0100

    Init

---
+++ DuckployTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DuckployTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
